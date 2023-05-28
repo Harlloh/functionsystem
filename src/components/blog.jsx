dd1a73e5c3426fa6419dd05e36dfc07f419f4335
femi <alloolorunfemi@gmail.com>
2023-05-28 21:56:37 +0100

    tried to make the blog page work

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -1,45 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import {NavLink} from 'react-router-dom';
-
-
-
+import { NavLink } from 'react-router-dom';
 
 const Blog = () => {
   const [blogPosts, setBlogPosts] = useState([]);
-
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          'https://newsapi.org/v2/everything',
-          {
-            params: {
-              q: 'crypto',
-              apiKey: 'ba86a3f42dc8447ea6906998d1494183',
-              pageSize: 10,
-            },
-          }
-        );
-        setBlogPosts(response.data.articles);
-      } catch (error) {
-        console.error('Error fetching blog posts:', error);
-      }
-    };
-
-    fetchData();
-
+  fetch('https://newsdata.io/api/1/news?apikey=pub_221745b3737ca9dd83968144054bcf7ed549b&q=crypto')
+  .then(res => res.json())
+  .then(data => {
+    console.log(data.results)
+    setBlogPosts(data.results)
+  })
+  .catch(error => {
+    // enter your logic for when there is an error (ex. error toast)
+   console.log(error)
+  })
   return (
     <div className="container newsss">
       <h2>Cryptocurrency News</h2>
       <ul>
         {blogPosts.map((post) => (
-          <NavLink className="overrr" to={post.url} key={post.url}>
-            <li>
-              {post.urlToImage && <img src={post.urlToImage} alt={post.title} />}
-              <h3>
-                {post.title}
-              </h3>
-            </li>
+          <NavLink href={post.link} key={post.link}>
+            {post.image_url && <img src={post.image_url} alt={post.summary} />}
+            <p >{post.title}</p>
           </NavLink>
         ))}
       </ul>
